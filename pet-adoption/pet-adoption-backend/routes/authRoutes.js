5f67596a949051bed0347c36b08a447c8bea89a3
basel-abdelazim <basel.abdelazim@gmail.com>
2024-10-22 08:50:10 +0200

    adoptionRoutes.js authRoutes.js AboutUs.jsx

--- pet-adoption/pet-adoption-backend/routes/authRoutes.js
+++ pet-adoption/pet-adoption-backend/routes/authRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.get('/users', getAllUsers);
 router.get('/users/:id', getUserById);
+router.get('/user', getUserById);
 router.post('/users', createUser);
 router.patch('/users/:id', updateUser);
 router.delete('/users/:id', deleteUser);
