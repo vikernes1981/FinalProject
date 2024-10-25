9ce711dd258eb8c8d36fb21218c8e235c04755e0
vikernes1981 <kostaspapas321@gmail.com>
2024-10-25 19:39:40 +0200

    Remove werver warnings

--- pet-adoption/pet-adoption-backend/server.js
+++ pet-adoption/pet-adoption-backend/server.js
@@ -30,10 +30,7 @@ app.use(cors({
 }));
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
